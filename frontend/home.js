539c915693d1f0b663e21d41e443afa94680a0bd
jérémie <permentierj@gmail.com>
2021-01-05 11:33:52 +0100

    Création de la page d'accueil avec tout les produits

---
+++ frontend/home.js
@@ -0,0 +1,82 @@
+// Création de l'instance XMLHttpRequest
+let request = new XMLHttpRequest;
+
+// Onreadystatechange permet de connaitre le statut de la requête
+request.onreadystatechange = function (){
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
+        let results = JSON.parse(request.responseText);
+       let elt = document.getElementById("products");
+        
+
+       // Création d'un objet pour le template
+        for(let i = 0; i < results.length; i++){
+            let card = {
+                container: document.createElement("div"),
+                row: document.createElement("div"),
+                link: document.createElement("a"),
+                img: document.createElement("img"),
+                title: document.createElement("h2"),
+                description: document.createElement("p"),
+                price: document.createElement("p"),
+                select: document.createElement("select")
+            };
+
+            // Ajout des éléments HTML
+            elt.appendChild(card.link);
+            card.link.appendChild(card.container);
+            card.container.appendChild(card.img);
+            card.container.appendChild(card.title);
+            card.container.appendChild(card.description);
+            card.container.appendChild(card.price);
+            card.container.appendChild(card.select);
+
+            // Boucle pour ajouter les option automatiquement
+            let option = 0;
+            while (option < results[i].lenses.length){
+                let opt = document.createElement("option");
+                card.select.appendChild(opt);
+                opt.textContent = results[i].lenses[option];
+                option ++;
+            };
+
+            
+            // Ajout des valeurs et des classes
+            elt.className = 'd-flex flex-wrap';
+            card.link.href = results[i]._id;
+            card.container.className = "card p-2";
+            card.img.src = results[i].imageUrl;
+            card.title.textContent = results[i].name;
+            card.description.textContent = results[i].description;
+            card.price.textContent = results[i].price / 100 + " €";
+        }
+
+        // function selection(selection){
+//     return `
+//     <select>
+//         ${selection.map(opt => `<option>${opt}</option>`).join('')}
+//     </select>
+//     `
+// }
+
+// function template(results){
+//     return `
+//     <div class="card">
+//     <img src="${results["imageUrl"]}" alt="${results["name"]}">
+//     <h2>${results["name"]}</h2>
+//     <p>${results["description"]}</p>
+//     <p>${results["price"]}</p>
+//     ${results["lenses"] ? selection(results["lenses"]) : ''};
+//     </div>
+//     `
+// }
+
+// document.getElementById("main").innerHTML =  
+// `
+// ${results.map(template).join(" ")} 
+// `
+// ;
+
+    }
+};
+request.open('GET', 'http://localhost:3000/api/cameras/', true);
+request.send();
